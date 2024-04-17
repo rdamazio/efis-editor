b4575485744f0b7a4366500a9c0ef4ee57c5a91a
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-17 01:19:49 -0700

    Add basic navigation structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ChecklistsComponent } from './checklists/checklists.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WelcomeComponent },
+    { path: 'checklists', component: ChecklistsComponent },
+    { path: 'about', component: AboutComponent },
+];
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    RouterOutlet,
+  ]
+})
+export class NavComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
