4d7eda1f94fce5a7cc2d90778533b8c2cf477b0b
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-19 04:24:52 -0700

    Fix test failures due to routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
