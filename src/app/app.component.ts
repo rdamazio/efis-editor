1f5cee956cacb24942651362f4fce0e35adec528
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-19 04:06:38 -0700

    Fix routing to serve from a subpath

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
