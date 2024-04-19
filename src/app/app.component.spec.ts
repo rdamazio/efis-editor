f3c3c15b9087bb90231ddd8b5f7ca6586113ba01
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-19 02:17:46 -0700

    Fixing basic tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        NoopAnimationsModule,
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +18,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'efis-editor' title`, () => {
+  it(`should have the 'EFIS Editor' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('efis-editor');
+    expect(app.title).toEqual('EFIS Editor');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, efis-editor');
+    expect(compiled.querySelector('mat-toolbar')?.textContent).toContain('EFIS Editor');
   });
 });
