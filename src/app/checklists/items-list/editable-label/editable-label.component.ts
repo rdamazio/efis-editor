ff0b31dc5b9bad5c1addd4b2f54add3eccabbc95
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-21 04:58:19 -0700

    Adding form validation to editable input

--- src/app/checklists/items-list/editable-label/editable-label.component.ts
+++ src/app/checklists/items-list/editable-label/editable-label.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,32 +16,34 @@ import { MatIconButtonSizesModule } from 'mat-icon-button-sizes';
     MatIconModule,
     MatInputModule,
     MatLabel,
+    ReactiveFormsModule,
   ],
   templateUrl: './editable-label.component.html',
   styleUrl: './editable-label.component.scss'
 })
 export class EditableLabelComponent {
-  private _value: string = '';
+  control = new FormControl('');
+  private _savedValue = '';
 
   @Output() valueChanged = new EventEmitter<string>();
   @Output() cancelled = new EventEmitter<boolean>();
   @Output() editing = false;
   @Input() label: string = '';
+  @Input() disallowEmpty = false;
 
   @Input()
-  get value(): string { return this._value; }
+  get value(): string { return this._savedValue; }
   set value(v: string) {
-    this._value = v;
-    this.onValueChanged();
+    this._savedValue = v;
+    this.control.setValue(v);
   }
 
-  onValueChanged() {
-    this.editing = false;
-    this.valueChanged.emit(this._value);
-  }
-
-  onCancelled() {
-    this.cancelled.emit(true);
+  save() {
+    if (this.editing) {
+      this.editing = false;
+      this._savedValue = this.control.value!;
+      this.valueChanged.emit(this._savedValue);
+    }
   }
 
   edit() {
@@ -48,6 +51,10 @@ export class EditableLabelComponent {
   }
 
   cancel() {
-    this.editing = false;
+    if (this.editing) {
+      this.editing = false;
+      this.control.setValue(this._savedValue);
+      this.cancelled.emit(true);
+    }
   }
 }
