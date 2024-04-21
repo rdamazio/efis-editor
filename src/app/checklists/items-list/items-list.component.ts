ab6381f738085c897759ff24018d2dc22ba0bdd4
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-21 02:14:20 -0700

    Basic checklist prompt editing

--- src/app/checklists/items-list/items-list.component.ts
+++ src/app/checklists/items-list/items-list.component.ts
@@ -1,6 +1,11 @@
 import { CdkDrag, CdkDragDrop, CdkDragHandle, CdkDragPlaceholder, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconButtonSizesModule } from 'mat-icon-button-sizes';
 import { Checklist, ChecklistItem, ChecklistItem_Type } from '../../../../gen/ts/checklist';
 
 @Component({
@@ -13,23 +18,39 @@ import { Checklist, ChecklistItem, ChecklistItem_Type } from '../../../../gen/ts
     CdkDragHandle,
     CdkDragPlaceholder,
     CdkDropList,
+    MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatIconButtonSizesModule,
+    MatIconModule,
+    MatInputModule,
+    MatLabel,
+    NgIf,
   ]
 })
 export class ChecklistItemsComponent {
   @Output() checklistChanged = new EventEmitter<Checklist>();
 
+  editing: boolean = false;
   _checklist?: Checklist;
   readonly ChecklistItem_Type = ChecklistItem_Type;
 
   @Input()
-  get checklist() : Checklist | undefined { return this._checklist; }
+  get checklist(): Checklist | undefined { return this._checklist; }
   set checklist(checklist: Checklist | undefined) {
     this._checklist = checklist;
   }
 
+  onIndent(item: ChecklistItem, delta: number) {
+    item.indent += delta;
+    this.checklistChanged.emit(this._checklist);
+  }
+
   onDrop(event: CdkDragDrop<ChecklistItem[]>): void {
     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  }
+
+  onItemUpdated() {
     this.checklistChanged.emit(this._checklist);
   }
 }
