19ecd46abe1a79c0f9963dbf19f2bfb5ba27e178
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-20 18:13:25 -0700

    Adding basic structure of main checklist contents

--- src/app/checklists/checklists.component.ts
+++ src/app/checklists/checklists.component.ts
@@ -1,12 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
 import { ChecklistTreeComponent } from './checklist-tree/checklist-tree.component';
 import { ChecklistFilePickerComponent } from './file-picker/file-picker.component';
+import { ChecklistItemsComponent } from './items-list/items-list.component';
 
 @Component({
   selector: 'app-checklists',
   standalone: true,
   imports: [
     ChecklistFilePickerComponent,
+    ChecklistItemsComponent,
     ChecklistTreeComponent
   ],
   templateUrl: './checklists.component.html',
