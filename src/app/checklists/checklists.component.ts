e158c1e07b54ec63bfa8ffdbd234f25da17f9f4f
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-04-26 03:55:50 -0700

    Add a file command bar and show file picker conditionally

--- src/app/checklists/checklists.component.ts
+++ src/app/checklists/checklists.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Checklist, ChecklistFile } from '../../../gen/ts/checklist';
 import { ChecklistStorage } from '../../model/storage/checklist-storage';
 import { ChecklistTreeComponent } from './checklist-tree/checklist-tree.component';
+import { ChecklistCommandBarComponent } from './command-bar/command-bar.component';
 import { ChecklistFilePickerComponent } from './file-picker/file-picker.component';
 import { ChecklistItemsComponent } from './items-list/items-list.component';
 
@@ -9,6 +10,7 @@ import { ChecklistItemsComponent } from './items-list/items-list.component';
   selector: 'app-checklists',
   standalone: true,
   imports: [
+    ChecklistCommandBarComponent,
     ChecklistFilePickerComponent,
     ChecklistItemsComponent,
     ChecklistTreeComponent
@@ -19,10 +21,52 @@ import { ChecklistItemsComponent } from './items-list/items-list.component';
 export class ChecklistsComponent {
   private _selectedFile?: ChecklistFile;
   @ViewChild("tree") tree?: ChecklistTreeComponent;
+  @ViewChild("filePicker") filePicker?: ChecklistFilePickerComponent;
+
+  showFilePicker: boolean = false;
 
   constructor(public store: ChecklistStorage) { }
 
+  onNewFile() {
+    this.showFilePicker = false;
+
+    let name = prompt("Enter a name for the new file:");
+    if (!name) {
+      return;
+    }
+
+    // Save an empty file with that name.
+    let file: ChecklistFile = {
+      name: name,
+      groups: [],
+    };
+    this.store.saveChecklistFile(file);
+    this._displayFile(file);
+  }
+
+  onOpenFile() {
+    this.showFilePicker = true;
+  }
+
+  onOpenFileCancel() {
+    this.showFilePicker = false;
+  }
+
+  onUploadFile() {
+    this.showFilePicker = false;
+
+    window.alert('TODO');
+  }
+
+  onDownloadFile() {
+    this.showFilePicker = false;
+
+    window.alert('TODO');
+  }
+
   onFileSelected(id: string) {
+    this.showFilePicker = false;
+
     let file: ChecklistFile | undefined;
     if (id) {
       let loadedFile = this.store.getChecklistFile(id);
@@ -30,8 +74,18 @@ export class ChecklistsComponent {
         file = loadedFile;
       }
     }
+    this._displayFile(file);
+
+    // TODO: Add filename to topbar, add rename pencil there
+  }
+
+  private _displayFile(file?: ChecklistFile) {
     this._selectedFile = file;
     this.tree!.file = file;
+    if (file) {
+      // Make the file selected the next time the picker gets displayed
+      this.filePicker!.selectedFile = file.name;
+    }
   }
 
   onStructureChanged(file: ChecklistFile) {
