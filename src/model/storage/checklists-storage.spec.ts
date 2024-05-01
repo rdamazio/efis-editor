c3a36ffcc8d96b555e10251b9ef58833c66f3727
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-05-01 16:42:53 -0700

    Add a test for deleting checklists.

--- src/model/storage/checklists-storage.spec.ts
+++ src/model/storage/checklists-storage.spec.ts
@@ -32,6 +32,11 @@ describe('ChecklistsService', () => {
     groups: [],
   };
 
+  const YET_ANOTHER_CHECKLIST_FILE: ChecklistFile = {
+    name: "Somethingwithoutspaces",
+    groups: [],
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     store = TestBed.inject(ChecklistStorage);
@@ -48,7 +53,7 @@ describe('ChecklistsService', () => {
   });
 
   describe('should save and read each checklist', () => {
-    [A_CHECKLIST_FILE, ANOTHER_CHECKLIST_FILE]
+    [A_CHECKLIST_FILE, ANOTHER_CHECKLIST_FILE, YET_ANOTHER_CHECKLIST_FILE]
       .forEach((file) => {
         beforeEach(() => {
           store.clear();
@@ -56,9 +61,12 @@ describe('ChecklistsService', () => {
 
         it('should save and read back a checklist', () => {
           store.saveChecklistFile(file);
-
           expect(store.listChecklistFiles()).toEqual([file.name]);
           expect(store.getChecklistFile(file.name)).toEqual(file);
+
+          store.deleteChecklistFile(file.name);
+          expect(store.listChecklistFiles()).toEqual([]);
+          expect(store.getChecklistFile(file.name)).toBeNull();
         });
       });
   });
@@ -66,12 +74,21 @@ describe('ChecklistsService', () => {
   it('should store and read multiple checklists', () => {
     store.saveChecklistFile(A_CHECKLIST_FILE);
     store.saveChecklistFile(ANOTHER_CHECKLIST_FILE);
+    store.saveChecklistFile(YET_ANOTHER_CHECKLIST_FILE);
     expect(store.listChecklistFiles()).toEqual(
       jasmine.arrayWithExactContents(
-        [A_CHECKLIST_FILE.name, ANOTHER_CHECKLIST_FILE.name]));
+        [A_CHECKLIST_FILE.name, ANOTHER_CHECKLIST_FILE.name, YET_ANOTHER_CHECKLIST_FILE.name]));
     expect(store.getChecklistFile(A_CHECKLIST_FILE.name)).toEqual(
       A_CHECKLIST_FILE);
     expect(store.getChecklistFile(ANOTHER_CHECKLIST_FILE.name)).toEqual(
       ANOTHER_CHECKLIST_FILE);
+    expect(store.getChecklistFile(YET_ANOTHER_CHECKLIST_FILE.name)).toEqual(
+      YET_ANOTHER_CHECKLIST_FILE);
+
+    store.deleteChecklistFile(ANOTHER_CHECKLIST_FILE.name);
+    expect(store.listChecklistFiles()).toEqual(
+      jasmine.arrayWithExactContents(
+        [A_CHECKLIST_FILE.name, YET_ANOTHER_CHECKLIST_FILE.name]));
+    expect(store.getChecklistFile(ANOTHER_CHECKLIST_FILE.name)).toBeNull();
   });
 });
