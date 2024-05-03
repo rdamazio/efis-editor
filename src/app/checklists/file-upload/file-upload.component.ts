736897707ce827c69ec293a2b6e93ea20c2c9ea4
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-05-03 02:36:39 -0700

    Adding a rudimentary way to parse uploads in various formats.

--- src/app/checklists/file-upload/file-upload.component.ts
+++ src/app/checklists/file-upload/file-upload.component.ts
@@ -3,6 +3,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { FileLikeObject, FileUploadModule, FileUploader } from 'ng2-file-upload';
 import { ChecklistFile } from '../../../../gen/ts/checklist';
 import { AceFormat } from '../../../model/formats/ace-format';
+import { GrtFormat } from '../../../model/formats/grt-format';
+import { FormatError } from '../../../model/formats/error';
+import { DynonFormat } from '../../../model/formats/dynon-format';
 
 @Component({
   selector: 'checklist-file-upload',
@@ -35,12 +38,30 @@ export class ChecklistFileUploadComponent {
 
   async onFile(files: File[]) {
     if (files) {
-      this.fileUploaded.emit(await AceFormat.toProto(files[0]));
+      const file = files[0];
+      // TODO: Come up with a nicer way to see which should match.
+      try {
+        this.fileUploaded.emit(await AceFormat.toProto(files[0]));
+      } catch (e) {
+        console.log('Failed to parse as ACE.');
+      }
+      try {
+        this.fileUploaded.emit(await GrtFormat.toProto(files[0]));
+      } catch (e) {
+        console.log('Failed to parse as GRT.');
+      }
+      try {
+        this.fileUploaded.emit(await DynonFormat.toProto(files[0]));
+      } catch (e) {
+        console.log('Failed to parse as Dynon.');
+      }
     }
   }
 
   private _fileExtensionFilter(item: FileLikeObject): boolean {
-    return !!item.name?.endsWith(".ace");
+    return !!item.name?.endsWith(".ace") ||
+           !!item.name?.endsWith(".txt") ||
+           !!item.name?.endsWith(".afd");
   }
 
 }
