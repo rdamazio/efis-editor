e2d4174160d6e0b380eb3aa04afb23db4b498fcd
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-05-06 21:02:14 -0700

    Switching to uploading with ngx-file-drop, using a drop zone.

--- src/app/checklists/file-upload/file-upload.component.ts
+++ src/app/checklists/file-upload/file-upload.component.ts
@@ -1,18 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { FileLikeObject, FileUploadModule, FileUploader } from 'ng2-file-upload';
+import { FileSystemFileEntry, NgxFileDropEntry, NgxFileDropModule } from 'ngx-file-drop';
 import { ChecklistFile } from '../../../../gen/ts/checklist';
 import { AceFormat } from '../../../model/formats/ace-format';
-import { GrtFormat } from '../../../model/formats/grt-format';
-import { FormatError } from '../../../model/formats/error';
 import { DynonFormat } from '../../../model/formats/dynon-format';
+import { GrtFormat } from '../../../model/formats/grt-format';
 
 @Component({
   selector: 'checklist-file-upload',
   standalone: true,
   imports: [
-    FileUploadModule,
     MatIconModule,
+    NgxFileDropModule,
   ],
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.scss'
@@ -21,47 +20,28 @@ export class ChecklistFileUploadComponent {
   @Output() fileUploaded = new EventEmitter<ChecklistFile>();
   @Output() cancel = new EventEmitter<ChecklistFile>();
   fileName: string = '';
-  uploader: FileUploader;
 
-  constructor() {
-    this.uploader = new FileUploader({
-      // We never upload to a server.
-      url: '',
-      filters: [
-        {
-          name: "extension",
-          fn: this._fileExtensionFilter,
+  onDropped(files: NgxFileDropEntry[]) {
+    for (const f of files) {
+      const fileEntry = f.fileEntry as FileSystemFileEntry;
+      fileEntry.file(async (file: File) => {
+        // TODO: Come up with a nicer way to see which should match.
+        try {
+          this.fileUploaded.emit(await AceFormat.toProto(file));
+        } catch (e) {
+          console.log('Failed to parse as ACE.');
         }
-      ],
-    });
-  }
-
-  async onFile(files: File[]) {
-    if (files) {
-      const file = files[0];
-      // TODO: Come up with a nicer way to see which should match.
-      try {
-        this.fileUploaded.emit(await AceFormat.toProto(files[0]));
-      } catch (e) {
-        console.log('Failed to parse as ACE.');
-      }
-      try {
-        this.fileUploaded.emit(await GrtFormat.toProto(files[0]));
-      } catch (e) {
-        console.log('Failed to parse as GRT.');
-      }
-      try {
-        this.fileUploaded.emit(await DynonFormat.toProto(files[0]));
-      } catch (e) {
-        console.log('Failed to parse as Dynon.');
-      }
+        try {
+          this.fileUploaded.emit(await GrtFormat.toProto(file));
+        } catch (e) {
+          console.log('Failed to parse as GRT.');
+        }
+        try {
+          this.fileUploaded.emit(await DynonFormat.toProto(file));
+        } catch (e) {
+          console.log('Failed to parse as Dynon.');
+        }
+      });
     }
   }
-
-  private _fileExtensionFilter(item: FileLikeObject): boolean {
-    return !!item.name?.endsWith(".ace") ||
-           !!item.name?.endsWith(".txt") ||
-           !!item.name?.endsWith(".afd");
-  }
-
 }
