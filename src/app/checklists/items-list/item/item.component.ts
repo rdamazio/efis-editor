76ab7fb34937ba695f8bb53fac799a62c2984776
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-05-10 22:29:06 -0700

    Make challenge/response edits be saved together

--- src/app/checklists/items-list/item/item.component.ts
+++ src/app/checklists/items-list/item/item.component.ts
@@ -1,6 +1,6 @@
 import { CdkDragHandle } from '@angular/cdk/drag-drop';
 import { NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatIconButtonSizesModule } from 'mat-icon-button-sizes';
@@ -27,20 +27,35 @@ export class ChecklistItemComponent {
   @Input() item!: ChecklistItem;
   @Output() itemChange = new EventEmitter<ChecklistItem>();
   @Output() itemDeleted = new EventEmitter<boolean>();
+  @ViewChild("promptInput") promptInput?: EditableLabelComponent;
+  @ViewChild("expectationInput") expectationInput?: EditableLabelComponent;
+
   readonly ChecklistItem_Type = ChecklistItem_Type;
 
   onIndent(item: ChecklistItem, delta: number) {
     item.indent += delta;
-    this.onItemUpdated(item);
+    this.onItemUpdated();
   }
 
   onCenterToggle(item: ChecklistItem) {
     item.indent = 0;
     item.centered = !item.centered;
-    this.onItemUpdated(item);
+    this.onItemUpdated();
   }
 
-  onItemUpdated(item: ChecklistItem) {
+  onSavePrompt() {
+    this.item.prompt = this.promptInput!.value;
+    this.onItemUpdated();
+    this.expectationInput!.save();
+  }
+
+  onSaveExpectation() {
+    this.item.expectation = this.expectationInput!.value;
+    this.onItemUpdated();
+    this.promptInput!.save();
+  }
+
+  onItemUpdated() {
     this.itemChange.emit(this.item);
   }
 
