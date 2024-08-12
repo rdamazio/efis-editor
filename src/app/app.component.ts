55299aa87b51385851015844e6bedfb7a786244d
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-08-12 12:23:23 -0700

    Using the newer Material Symbols font instead of the older Material Icons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent],
+  imports: [MatIconModule, NavComponent],
   template: '<app-nav />',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EFIS Editor';
+
+  constructor(private matIconRegistry: MatIconRegistry) {}
+
+  ngOnInit(): void {
+    this.matIconRegistry.setDefaultFontSetClass('material-symbols-outlined');
+  }
 }
