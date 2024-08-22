e7c33987ea4dddec90949e9d6d65fe76bebaf078
Yury V. Zaytsev <yury@shurup.com>
2024-08-22 19:13:31 +0200

    fix(model): use uuid v4 directly as foreflight object ids

--- src/model/formats/foreflight-format.spec.ts
+++ src/model/formats/foreflight-format.spec.ts
@@ -5,12 +5,23 @@ import { ForeFlightReader } from './foreflight-reader';
 import { loadFile } from './test-utils';
 import { ForeFlightWriter } from './foreflight-writer';
 import { ForeFlightChecklistContainer } from '../../../gen/ts/foreflight';
+import { validate } from 'uuid';
 
 describe('ForeFlightFormat', () => {
   describe('ForeFlightUtils', () => {
-    it(`should generate ${ForeFlightUtils.OBJECT_ID_LENGTH}-characters long alphanumeric IDs`, async () => {
-      const objectId = await ForeFlightUtils.getObjectId();
-      expect(objectId).toMatch(`[a-z0-9]{${ForeFlightUtils.OBJECT_ID_LENGTH}}`);
+    it(`should generate uuid v4 objectIds w/o dashes`, async () => {
+      const objectId = ForeFlightUtils.getObjectId();
+
+      const groupSizes = [8, 4, 4, 4, 12];
+      const groupOctets: string[] = [];
+
+      groupSizes.reduce((previousValue, currentValue) => {
+        groupOctets.push(objectId.slice(previousValue, previousValue + currentValue));
+        return previousValue + currentValue;
+      }, 0);
+
+      expect(objectId).toMatch(`[a-f0-9]{12}4[a-f0-9]{19}`); // check case and v4
+      expect(validate(groupOctets.join('-'))).toBe(true);
     });
 
     it('should encrypt and decrypt data passing round-trip', async () => {
