89a2c6e9598431d19199c0791c75552f6380d3b8
Yury V. Zaytsev <yury@shurup.com>
2024-08-23 12:03:14 +0200

    feat(checklist-tree): implement editing group categories

--- src/app/checklists/checklist-tree/node/node.component.ts
+++ src/app/checklists/checklist-tree/node/node.component.ts
@@ -2,15 +2,21 @@ import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatOption, MatSelect } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconButtonSizesModule } from 'mat-icon-button-sizes';
 import { ChecklistGroup_Category } from '../../../../../gen/ts/checklist';
 import { ChecklistTreeNode } from './node';
 
+interface CategorySelector {
+  viewValue: string;
+  style: string;
+}
+
 @Component({
   selector: 'checklist-tree-node',
   standalone: true,
-  imports: [MatButtonModule, MatIconButtonSizesModule, MatIconModule, MatTooltipModule, NgIf],
+  imports: [MatButtonModule, MatIconButtonSizesModule, MatIconModule, MatTooltipModule, NgIf, MatSelect, MatOption],
   templateUrl: './node.component.html',
   styleUrl: './node.component.scss',
 })
@@ -20,5 +26,17 @@ export class ChecklistTreeNodeComponent {
   @Output() nodeDelete = new EventEmitter<ChecklistTreeNode>();
 
   hideButtons = true;
-  protected readonly ChecklistGroup_Category = ChecklistGroup_Category;
+
+  protected readonly checklistGroupCategories = new Map<ChecklistGroup_Category, CategorySelector>([
+    [ChecklistGroup_Category.normal, { viewValue: '🄽', style: 'color: white !important' }],
+    [ChecklistGroup_Category.abnormal, { viewValue: '🄰', style: 'color: yellow !important' }],
+    [ChecklistGroup_Category.emergency, { viewValue: '🄴', style: 'color: red !important' }],
+  ]);
+
+  get checklistGroupCategory(): ChecklistGroup_Category {
+    return this.node.group!.category;
+  }
+  set checklistGroupCategory(value: ChecklistGroup_Category) {
+    this.node.group!.category = value;
+  }
 }
