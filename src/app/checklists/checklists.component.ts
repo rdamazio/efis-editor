2ae3dbf946851e2bb2fa4bb19fe9528bc5f1bf31
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-09-01 00:46:34 -0700

    Adding a top bar icon for keyboard shortcut help

--- src/app/checklists/checklists.component.ts
+++ src/app/checklists/checklists.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe } from '@angular/common';
-import { afterNextRender, Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { afterNextRender, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HotkeysDirective, HotkeysService } from '@ngneat/hotkeys';
@@ -53,7 +53,6 @@ export class ChecklistsComponent implements OnInit, OnDestroy {
 
   showFilePicker = false;
   showFileUpload = false;
-  helpRef?: MatDialogRef<HelpComponent>;
 
   constructor(
     public store: ChecklistStorage,
@@ -67,19 +66,7 @@ export class ChecklistsComponent implements OnInit, OnDestroy {
       this._hotkeys.setSequenceDebounce(500);
 
       this._hotkeys.registerHelpModal(() => {
-        if (this.helpRef) {
-          this.helpRef.close();
-          return;
-        }
-
-        this.helpRef = this._dialog.open(HelpComponent, {
-          hasBackdrop: true,
-          width: '500px',
-        });
-
-        this.helpRef.afterClosed().subscribe(() => {
-          this.helpRef = undefined;
-        });
+        HelpComponent.toggleHelp(_dialog);
       });
 
       this._hotkeys
--- src/app/checklists/hotkeys/help/help.component.ts
+++ src/app/checklists/hotkeys/help/help.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {
+  MatDialog,
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
@@ -29,11 +30,28 @@ import { ShortcutPipe } from './shortcut.pipe';
 export class HelpComponent {
   DISPLAYED_COLUMNS = ['action', 'key'];
   hotkeys: HotkeyGroup[];
+  private static _helpRef?: MatDialogRef<HelpComponent>;
 
   constructor(
     public dialogRef: MatDialogRef<HelpComponent>,
     hotkeysService: HotkeysService,
   ) {
     this.hotkeys = hotkeysService.getShortcuts();
+  }
+
+  static toggleHelp(dialog: MatDialog) {
+    if (this._helpRef) {
+      this._helpRef.close();
+      return;
+    }
+
+    this._helpRef = dialog.open(HelpComponent, {
+      hasBackdrop: true,
+      width: '500px',
+    });
+
+    this._helpRef.afterClosed().subscribe(() => {
+      this._helpRef = undefined;
+    });
   }
 }
