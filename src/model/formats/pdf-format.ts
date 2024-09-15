9c609f56af53b8ae3ae2cf5cb8b54c49c341bbf6
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-09-14 19:37:06 -0700

    pdf(#68): Adding a footer to the cover page

--- src/model/formats/pdf-format.ts
+++ src/model/formats/pdf-format.ts
@@ -5,6 +5,7 @@ export class PdfFormat {
   // TODO: Add more PDF format options (popup dialog?)
   private static PDF_OPTIONS: PdfWriterOptions = {
     outputCoverPage: true,
+    outputCoverPageFooter: true,
   };
 
   public static async fromProto(file: ChecklistFile): Promise<File> {
