2bb99472620d6593a5aa5c1f778a311a3c559280
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-09-23 03:27:00 -0700

    shortcuts(#73): temporarily disabling Option translation test, which only passes on macOS

--- src/app/checklists/hotkeys/help/shortcut.pipe.spec.ts
+++ src/app/checklists/hotkeys/help/shortcut.pipe.spec.ts
@@ -7,7 +7,8 @@ describe('ShortcutPipe', () => {
 
     expect(pipe.transform('shift.up')).toEqual('&#8679; + &#8593;');
     expect(pipe.transform('shift.ArrowDown')).toEqual('&#8679; + &#8595;');
-    expect(pipe.transform('alt.up')).toEqual('&#8997; + &#8593;');
+    // TODO: Make this only run on macOS (or mock the OS detection for it).
+    // expect(pipe.transform('alt.up')).toEqual('&#8997; + &#8593;');
     expect(pipe.transform('up>ArrowUp>down>ArrowDown>left>right>ArrowLeft>ArrowRight>B>A')).toEqual(
       '&#8593; then &#8593; then &#8595; then &#8595; then &#8592; then &#8594; then &#8592; then &#8594; then b then a',
     );
