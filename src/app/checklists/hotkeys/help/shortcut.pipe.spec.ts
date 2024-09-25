5a328789b747ea82d86a4ba195bfecf3232f024d
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-09-25 12:54:44 -0700

    shortcuts(#73): re-enabling macOS-specific shortcut translation test

--- src/app/checklists/hotkeys/help/shortcut.pipe.spec.ts
+++ src/app/checklists/hotkeys/help/shortcut.pipe.spec.ts
@@ -1,16 +1,34 @@
 import { ShortcutPipe } from './shortcut.pipe';
 
 describe('ShortcutPipe', () => {
-  it('', () => {
+  it('should perform expected transformations', () => {
     const pipe = new ShortcutPipe();
     expect(pipe).toBeTruthy();
 
     expect(pipe.transform('shift.up')).toEqual('&#8679; + &#8593;');
     expect(pipe.transform('shift.ArrowDown')).toEqual('&#8679; + &#8595;');
-    // TODO: Make this only run on macOS (or mock the OS detection for it).
-    // expect(pipe.transform('alt.up')).toEqual('&#8997; + &#8593;');
     expect(pipe.transform('up>ArrowUp>down>ArrowDown>left>right>ArrowLeft>ArrowRight>B>A')).toEqual(
       '&#8593; then &#8593; then &#8595; then &#8595; then &#8592; then &#8594; then &#8592; then &#8594; then b then a',
     );
+  });
+
+  it('should perform Mac-specific transformations', () => {
+    spyOnProperty(window.navigator, 'userAgent').and.returnValue(
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) Chrome/129.0.0.0',
+    );
+    const pipe = new ShortcutPipe();
+    expect(pipe).toBeTruthy();
+
+    expect(pipe.transform('alt.up')).toEqual('&#8997; + &#8593;');
+  });
+
+  it('should perform non-Mac transformations', () => {
+    spyOnProperty(window.navigator, 'userAgent').and.returnValue(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/129.0.0.0',
+    );
+    const pipe = new ShortcutPipe();
+    expect(pipe).toBeTruthy();
+
+    expect(pipe.transform('alt.up')).toEqual('Alt + &#8593;');
   });
 });
