9c994a683a4edb55c857f3d2f226f5057ca2fe70
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2024-11-02 00:37:26 -0700

    gdrive: Connecting gDrive sync to UI.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,8 +8,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HotkeysService } from '@ngneat/hotkeys';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { DriveSyncState, GoogleDriveStorage } from '../../model/storage/gdrive';
 import { AboutComponent } from '../about/about.component';
 import { HelpComponent } from '../checklists/hotkeys/help/help.component';
 
@@ -20,7 +21,6 @@ import { HelpComponent } from '../checklists/hotkeys/help/help.component';
   styleUrl: './nav.component.scss',
   standalone: true,
   imports: [
-    AsyncPipe,
     MatButtonModule,
     MatDialogModule,
     MatIconModule,
@@ -31,6 +31,7 @@ import { HelpComponent } from '../checklists/hotkeys/help/help.component';
     RouterOutlet,
   ],
 })
+@UntilDestroy()
 export class NavComponent {
   private readonly _breakpointObserver = inject(BreakpointObserver);
 
@@ -39,10 +40,17 @@ export class NavComponent {
     shareReplay(),
   );
 
+  cloudIconDisabled = false;
+  cloudIcon = 'cloud_off';
+
   constructor(
     protected hotkeys: HotkeysService,
     private readonly _dialog: MatDialog,
-  ) {}
+    private readonly _gdrive: GoogleDriveStorage,
+    private readonly _changeDet: ChangeDetectorRef,
+  ) {
+    this._gdrive.getState().pipe(untilDestroyed(this)).subscribe(this._updateCloudUi.bind(this));
+  }
 
   showAbout() {
     this._dialog.open(AboutComponent, {
@@ -58,5 +66,33 @@ export class NavComponent {
     }
 
     HelpComponent.toggleHelp(this._dialog);
+  }
+
+  synchronizeToCloud() {
+    void this._gdrive.synchronize();
+  }
+
+  private _updateCloudUi(state: DriveSyncState) {
+    this.cloudIconDisabled = state === DriveSyncState.SYNCING;
+
+    switch (state) {
+      case DriveSyncState.DISCONNECTED:
+        this.cloudIcon = 'cloud_off';
+        break;
+      case DriveSyncState.SYNCING:
+        this.cloudIcon = 'cloud_sync';
+        break;
+      case DriveSyncState.NEEDS_SYNC:
+        this.cloudIcon = 'cloud_upload';
+        break;
+      case DriveSyncState.IN_SYNC:
+        this.cloudIcon = 'cloud_done';
+        break;
+      case DriveSyncState.FAILED:
+        this.cloudIcon = 'cloud_alert';
+        break;
+    }
+
+    this._changeDet.markForCheck();
   }
 }
