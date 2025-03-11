1ec9a1932d39f064a517c0e9df50f43436f80cd2
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2025-03-11 12:32:08 -0700

    offline: ng add @angular/pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ROUTES } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const APP_CONFIG: ApplicationConfig = {
   providers: [
     provideRouter(ROUTES, withPreloading(PreloadAllModules)),
     provideClientHydration(withIncrementalHydration()),
     provideAnimationsAsync(),
-    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 5000 } },
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 5000 } }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
