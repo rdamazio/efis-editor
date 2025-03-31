407736c5b880fc30d675fd3034f1b2f374a13824
Yury V. Zaytsev <yury@shurup.com>
2025-03-31 09:17:05 +0200

    feat(welcome): use format registry in welcome to DRY
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
+import { FORMAT_REGISTRY } from '../../model/formats/format-registry';
 
 @Component({
   selector: 'app-welcome',
@@ -9,4 +10,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  protected readonly _formatRegistry = FORMAT_REGISTRY;
+}
