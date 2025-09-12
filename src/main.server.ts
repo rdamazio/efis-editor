e6cfa96b4f23f8d259fa26dada0bfbd6538aa437
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2025-09-12 13:47:00 -0700

    build(deps): Updating server init after Angular breaking change

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { CONFIG } from './app/app.config.server';
 
-const bootstrap = async () => bootstrapApplication(AppComponent, CONFIG);
+const bootstrap = async (context: BootstrapContext) => bootstrapApplication(AppComponent, CONFIG, context);
 
 export default bootstrap;
