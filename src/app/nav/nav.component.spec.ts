a08f044aee02f1dace7fab4b07c7e418d1c97f76
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2025-11-08 14:19:16 -0800

    testing: use builtin parameter for hydration

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -38,6 +38,7 @@ describe('NavComponent', () => {
         },
         provideClientHydration(withIncrementalHydration()),
       ],
+      deferBlockStates: DeferBlockState.Complete,
     });
     fixture = rendered.fixture;
     navData = fixture.componentInstance.navData;
