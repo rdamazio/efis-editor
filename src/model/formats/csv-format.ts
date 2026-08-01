32bf25a415051f8ca9a872b939c4e5ab64c1618a
Yury V. Zaytsev <yury@shurup.com>
2026-08-01 15:26:32 +0200

    feat(formats): functional rewrite of the CSV import/export

--- src/model/formats/csv-format.ts
+++ src/model/formats/csv-format.ts
@@ -2,15 +2,6 @@ import { ChecklistFile } from '../../../gen/ts/checklist';
 import { AbstractChecklistFormat } from './abstract-format';
 import { CsvReader } from './csv-reader';
 import { CsvWriter } from './csv-writer';
-import { FormatError } from './error';
-
-export class CsvFormatError extends FormatError {
-  constructor(message: string, cause?: Error) {
-    super(`CSV: ${message}`);
-    this.cause = cause;
-    this.name = 'CsvFormatError';
-  }
-}
 
 export class CsvFormat extends AbstractChecklistFormat {
   public async toProto(file: File): Promise<ChecklistFile> {
@@ -18,7 +9,6 @@ export class CsvFormat extends AbstractChecklistFormat {
   }
 
   public async fromProto(file: ChecklistFile): Promise<File> {
-    const blob = CsvWriter.write(file);
-    return await Promise.resolve(new File([blob], `${file.metadata!.name}${this.extension}`));
+    return Promise.resolve(new File([CsvWriter.write(file)], `${file.metadata!.name}${this.extension}`));
   }
 }
